fa15569e651820e2c7e8a08f720b5cd6426db567
tung_tvt <trinhvutung1987@gmail.com>
2019-12-09 10:44:44 +0700

    Merge

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,21 +1,15 @@
 package hello;
 
-import com.google.api.core.ApiFuture;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.capillary.Config;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.cloud.FirestoreClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
@@ -29,13 +23,6 @@ public class Application {
     private static final String TLS_PRIVATE_KEY_PATH_OPTION = "tls_private_key_path";
     private static final String SERVICE_ACCOUNT_CREDENTIALS_PATH_OPTION = "service_account_credentials_path";
     private static final String FIREBASE_PROJECT_ID_OPTION = "firebase_project_id";
-
-    private static List<QueryDocumentSnapshot> mUserData;
-
-//    @RequestMapping("/")
-//    public String home() {
-//        return "Hello Docker World";
-//    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
@@ -56,22 +43,10 @@ public class Application {
                     .build();
             FirebaseApp.initializeApp(options);
             System.out.println("Init Firebase success");
-            getDataFromFireStore();
         } catch (IOException e) {
             System.out.println("ERROR: invalid service account credentials. See README.");
             System.out.println(e.getMessage());
             System.exit(1);
-        }
-    }
-
-    private static void getDataFromFireStore() throws ExecutionException, InterruptedException {
-        Firestore db = FirestoreClient.getFirestore();
-        ApiFuture<QuerySnapshot> query = db.collection("users").get();
-        QuerySnapshot querySnapshot = query.get();
-        mUserData = querySnapshot.getDocuments();
-
-        for (QueryDocumentSnapshot document : mUserData) {
-            System.out.println("TVT email = " + document.getData().get("email"));
         }
     }
 
--- complete/src/main/java/hello/UserListController.java
+++ complete/src/main/java/hello/UserListController.java
@@ -1,15 +1,40 @@
 package hello;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 @Controller
 public class UserListController {
+
+    private static List<QueryDocumentSnapshot> mUserData;
+
     @GetMapping("/userlist")
-    public String userlist(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String userlist(@RequestParam(name = "email", required = false, defaultValue = "World") String email, Model model) throws ExecutionException, InterruptedException {
+        return getDataFromFireStore(model);
+    }
+
+    private static String getDataFromFireStore(Model model) throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> query = db.collection("users").get();
+        QuerySnapshot querySnapshot = query.get();
+        mUserData = querySnapshot.getDocuments();
+
+        for (QueryDocumentSnapshot document : mUserData) {
+            System.out.println("TVT email = " + document.getData().get("email"));
+            model.addAttribute("email", document.getData().get("email"));
+        }
+
         return "userlist";
     }
 }
